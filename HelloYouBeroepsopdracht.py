95615d083f8291e24004bbf30cbbdef0c31870e9
Joey <32838@ma-web.nl>
2021-11-05 12:44:43 +0100

    first commit

---
+++ HelloYouBeroepsopdracht.py
@@ -0,0 +1,182 @@
+
+import os 
+import time
+print ("Hello, mijn naam is Taco Zwakofzki")
+print ("Wat is jou naam?")
+
+#voorstelling (stukje1)
+
+username = input("Enter username:")
+print ("Hallo: " + username)
+print ("Ik ga jou mijn verhaal uitleggen en je kan een paar keuzes maken maak de juiste keuzes!")
+
+print ("")
+print ("Hallo, wij zijn Kawod En Taco Zwakofzki. Wij zijn broers. Kawod is 40, en Taco is 39.Wij komen oorspronkelijk uit Irak, maar daar moesten wij helaas weg. Wij moesten onze ouders en andere familieleden helaas achterlaten. Zodat wij het nog konden overleven, en ons leven voort kunnen zetten. Dit is de kant van Taco")
+
+#Stukje2
+print ("Het was 20 maart toen het begon, de oorlog tegen terrorisme wij hadden er niks mee te maken en hoopte dat de oorlog ons zo min mogelijk zou beïnvloeden en dat wij in ons land konden blijven maar tevergeefs de bombardementen waren gestart en wij hebben veel familie en vrienden verloren toen moesten wij echt weg. maar wel vervoer gaan we nemen zegt Taco tegen Kawod we kunnen lopen maar dat is wel heel gevaarlijk, we kunnen de taxi nemen maar dat is wel heel duur en we kunnen fietsen alleen dan zijn we wel heel moe als we daar aankomen")
+print (" ")
+print ("A Kawod en Taco gaan lopen naar het vliegveld")
+print ("B Kawod en Taco gaan met de taxi naar het vliegveld")
+print ("C Kawod en Taco gaan fietsen naar het vliegveld")
+
+#Stukje3
+scriptinput = input()
+if scriptinput == "A" or scriptinput == "a":
+    os.system("cls")
+    print ("")
+    print ("Kawod en Taco  gaan lopen naar het vliegveld maar uiteindelijk belanden ze midden in een vuurgevecht ze schuilen snel achter een muurtje en wachten totdat het voorbij is taco kijkt opzij en ziet Kawod met ze ogen dicht en ze oren dicht in de hoek van de kamer zitten het gevecht is voorbij en je tikt Kawod aan, kom we gaan door je kijkt naar buiten en het ziet er veilig uit.")
+    print ("")
+    print ("Druk op een knop om door te gaan")
+    #stukje4
+    scriptinput = input()
+if scriptinput == "B" or scriptinput == "b":
+    os.system("cls")
+    print ("")
+    print ("Kawlod en Taco gaan met de taxi naar het vliegveld het is een aardige bestuurder die goed weet wat die doet je hebt het met hem over zijn familie en de situatie in het land en het is erg gezellig uiteindelijk zijn jullie halverwege en zijn jullie uitgepraat het is heel even stil maar uiteindelijk zet de bestuurder een muziekje aan. je bent nu op het vliegveld de rit was erg duur en jullie zijn nu een deel van jullie budget kwijt.")
+    
+    #stukje5
+    scriptinput = input()
+if scriptinput == "C" or scriptinput == "c":
+    os.system("cls")
+    print ("")
+    print ("Kawod en Taco gaan fietsen naar het vliegveld jullie conditie is niet erg goed maar het kan ermee door jullie beginnen met fietsen en praten jullie hebben het over dat jullie de familie erg gaan missen en hopen dat het goed met hun komt halverwege krijgt Taco een lekke band maar gelukkig weet Kawod hoe die je band plakt dus jullie kunnen door ")
+    
+    script = True
+
+    #stukje6
+print ("je bent nu op het vliegveld en er gaat over een uur een vlucht naar rusland een mooi en groot land en er gaat over 2 uur een vlucht naar nederland een klein maar welvarend land er over 5 min gaat er een vlucht naar ibiza een mooi vakantie land")
+print (" ")
+print ("A Taco gaat naar Nederland en Kawod naar Rusland ")
+print ("B Taco gaat naar rusland en Kawod gaat naar nederland")
+print ("C Ze gaan samen naar ibiza")
+
+    #stukje 7 (NL)
+scriptinput = input()
+if scriptinput == "A" or scriptinput == "a":
+    os.system("cls")
+    print ("")
+    print ("Goed het is ondertussen een uur later en Kawod neemt het vliegtuig naar rusland je zwaait je broer uit en je laat een traantje. eindelijk is jou vliegtuig er en je gaat op weg naar nederland maar je kent de taal nog helemaal niet je maakt je daar een beetje druk om maar je bent ook moe na de lange vlucht uiteindelijk beslist Taco om te gaan slapen omdat die niet goed kan leren als die moe is hij word wakker en hij is er bijna nu moet die kijken voor een hotel hij heeft een keuze tussen een budget kamer of een deluxe kamer maar welke zal die kiezen")
+    print ("A Taco neemt de Budget kamer")
+    print ("B Taco neemt de deluxe kamer")
+    #stukje 10 (NL)
+    scriptinput = input()
+    if scriptinput == "A" or scriptinput == "a":
+        print("")
+        print ("Goed taco neemt de budget kamer hij komt aan in het hotel en beneden ziet het er nog best goed uit als je de vuile was op de grond en de dode rat in de hoek even weg denkt, hij vraagt aan de balie naar zijn kamer sleutel en gaat naar zijn kamer. hij loopt naar binnen en schrikt het stinkt super erg en zijn bed is totaal niet opgemaakt maar op dit moment maakt het hem niet uit hij is moe en wilt slapen. hij slaapt niet lekker en word wakker met rugpijn maar dat is een probleem voor later")
+        #stukje 12 (NL)
+        time.sleep(10)
+        os.system("cls")
+        print ("Taco word wakker en hij heeft last van zijn rug hij gaat naar beneden om bij de balie te vragen of ze wat medicatie hebben ze geven hem een raar pilletje waarvan hij geen idee heeft wat het is maar hij neemt hem toch hij gaat terug naar zijn kamer en gaat op zijn telefoon hij moet een baan fixen hij heeft geld nodig zodat die normaal kan leven maar hij wilt liever thuis blijven dus nu moet die een keuze maken")
+        print ("A Opzoek gaan naar werk")
+        print ("B binnen blijven en niks doen")
+        #Stukje 14 (NL)
+        scriptinput == input ()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system("cls")
+            print ("")
+            print ("Taco beslist toch opzoek te gaan naar werk hij gaat naar buiten en geniet eerst even van het mooie uitzicht hij loopt een beetje door de stad en kijkt rond, hij is blij met zijn beslissing en wilt eig dat zijn broer ook komt. hij gaat naar een uitzendings bureau opzoek naar een baan ")
+
+        #stukje 15 (NL)
+        scriptinput == input ()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system ("cls")
+            print ("Taco blijft in het hotel en doet de rest van de dag niks hij zit een beetje te chillen en kijkt wat tv uiteindelijk krijgt hij honger en hij wilt ergens wat gaan eten maar aangezien hij de heledag in zijn kamer heeft besteed weet hij niet waar die naartoe moet hij beslist maar roomservice te bestellen Taco beslist te gaan slapen en de volgende dag naar het uitzendings bureau te gaan en een baan te nemen maar nu is de vraag welke baan.")
+
+    
+    scriptiput = input ()
+    if scriptinput == "B" or scriptinput == "b":
+        os.system("cls")
+        print ("")
+        print ("Taco komt aan bij een super mooi hotel er zit overal goud en het is super netjes ze bagage wordt meteen meegenomen en naar zijn kamer gebracht, hij loopt naar de balie en vraagt voor de sleutel de mevrouw reageert erg aardig en wijst hem ook direct de weg. hij komt aan bij zijn kamer en valt bijna om. het is super netjes en mooi en het bed is super groot zonder na te denken sprint taco het bed in en hij zou verrukkelijk slapen.")
+        #stukje 13
+        time.sleep (10)
+        os.system("cls")
+        print ("Taco word wakker en hij voelt zich super goed hij heeft erg lekker geslapen op dit super grote bed hij stapt uit bed en rekt zich uit en stapt vervolgens meteen onder de douche wanneer hij klaar is met douchen doet die zijn kleding weer aan en beseft dat het super vies is en hij heeft nieuwe nodig hij wilt eigenlijk zo lang mogelijk in dit hotel blijven maar hij moet ook echt werk zoeken nu moet die een keuze maken. ")
+        print ("A Opzoek gaan naar werk")
+        print ("B binnen blijven en niks doen")
+        
+        #Stukje 14 (NL)
+        scriptinput == input ()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system("cls")
+            print ("")
+            print ("Taco beslist toch opzoek te gaan naar werk hij gaat naar buiten en geniet eerst even van het mooie uitzicht hij loopt een beetje door de stad en kijkt rond, hij is blij met zijn beslissing en wilt eig dat zijn broer ook komt. hij gaat naar een uitzendings bureau opzoek naar een baan ")
+
+        #stukje 15 (NL)
+        scriptinput == input ()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system ("cls")
+            print ("Taco blijft in het hotel en doet de rest van de dag niks hij zit een beetje te chillen en kijkt wat tv uiteindelijk krijgt hij honger en hij wilt ergens wat gaan eten maar aangezien hij de heledag in zijn kamer heeft besteed weet hij niet waar die naartoe moet hij beslist maar roomservice te bestellen Taco beslist te gaan slapen en de volgende dag naar het uitzendings bureau te gaan en een baan te nemen maar nu is de vraag welke baan.")
+    #stukje 16 einde (NL)
+    time.sleep (15)
+    print ("Taco heeft gekozen voor Vakkenvuller hij krijgt te horen dat die volgende week moet beginnen en begint hem toch wel druk te maken hij kent de taal nog niet en vind het dus erg lastig ondertussen is het een maand later en hij snapt nederlands nu al best goed hij heeft ondertussen zijn eigen appartement en nog contact met zijn familie (EINDE)")
+        
+    
+    #stukje 7 (rus)
+    scriptinput = input()
+if scriptinput == "B" or scriptinput == "b":
+    os.system("cls")
+    print ("")
+    print ("Mijn vliegtuig is er en ik zie Kawod mij uitzwaaien ik denk er ff over na om toch terug te gaan omdat ik niet mijn broer wil achterlaten maar ik moet wel. ik zit in het vliegtuig en ik maak me zorgen over Kawod ik hoop dat die ze vliegtuig haalt en niet word gepakt op het vliegveld maar ik moet me nu zorgen maken over de toekomst. waar ga ik overnachten ik heb familie in rusland en ik moet nu kiezen bij wie ik ga blijven mijn opa van mijn vaders kant of mijn opa en oma van me moeders kant")
+    print ("A Opa van vaders kant")
+    print ("B Opa en Oma van Moeders kant")
+
+    scriptinput = input ()
+    if scriptinput == "A" or scriptinput == "a":    
+        os.system ("cls")
+        print ("Goed ik kies maar voor me opa van mijn vaders kant die ken ik wat beter maar ik weet nog niet hoe ik daar naar toe moet ik kan wel lopen maar ik weet niet goed de weg hier gelukkig heb ik google maps maar ik kan ook gwn makkelijk de taxi nemen ")
+        print ("A Lopen")
+        print ("B Taxi")
+
+        jscriptinput = input ()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system ("cls")
+            print ("Ik denk dat lopen het handigste is het is goedkoper en dan kan ik ook meteen wat van de stad zien het zal alleen wel even duren er staat hier dat er hier een shortcut is alleen het is wel een eng klein steegje ik weet niet zeker of ik dit wel moet doen")
+            print ("A wel steegje nemen")
+            print ("B Niet het steegje nemen")
+            scriptinput = input ()
+            if scriptinput == "A" or scriptinput == "a":
+                 print ("je beslist toch het steegje te nemen je loopt er langzaam in en je pakt je telefoon in de hoop dat niemand je lastig valt totdat je uiteindelijk iemand hoort roepen je draait je om en je ziet iemand met een wapen allemaal russische dingen naar je schreeuwen je probeert hem duidelijk te maken dat je het niet verstaat en dan hoor je een knal en val je neer…...Eind")
+
+            scriptinput = input ()
+            if scriptinput == "B" or scriptinput == "b":
+                print ("Je neemt het donkere steegje toch maar niet je vind het iets te sketchy uiteindelijk kom je aan bij je opa en oma hun zijn super blij om je te zien en zeggen dat je mag blijven voor hoelang je ook maar wilt…..Eind")
+
+
+    scriptinput = input ()
+    if scriptinput == "B" or scriptinput == "b":
+        os.system ("cls")
+        print ("Nou dan maar me opa en oma van mijn moeders kant ik heb veel goeds over ze gehoord van me moeder maar heb ze helaas nog nooit mogen ontmoeten, nu is de vraag hoe ga ik daar komen zal ik lopen en met google maps of doe ik makkelijk en bel ik een taxi ")
+        print ("A Lopen")
+        print ("B Taxi")
+
+        scriptinput = input ()
+        if scriptinput == "A" or scriptinput == "a":
+            os.system ("cls")
+            print ("Ik denk dat lopen het handigste is het is goedkoper en dan kan ik ook meteen wat van de stad zien het zal alleen wel even duren er staat hier dat er hier een shortcut is alleen het is wel een eng klein steegje ik weet niet zeker of ik dit wel moet doen")
+            print ("A wel steegje nemen")
+            print ("B Niet het steegje nemen")
+            scriptinput = input ()
+            if scriptinput == "A" or scriptinput == "a":
+                 print ("je beslist toch het steegje te nemen je loopt er langzaam in en je pakt je telefoon in de hoop dat niemand je lastig valt totdat je uiteindelijk iemand hoort roepen je draait je om en je ziet iemand met een wapen allemaal russische dingen naar je schreeuwen je probeert hem duidelijk te maken dat je het niet verstaat en dan hoor je een knal en val je neer…...Eind")
+
+            scriptinput = input ()
+            if scriptinput == "B" or scriptinput == "b":
+                print ("Je neemt het donkere steegje toch maar niet je vind het iets te sketchy uiteindelijk kom je aan bij je opa en oma hun zijn super blij om je te zien en zeggen dat je mag blijven voor hoelang je ook maar wilt…..Eind")
+
+
+        scriptinput = input ()
+        if scriptinput == "B" or scriptinput == "b":
+            os.system ("cls")
+            print ("Het is echt super koud ik denk dat het beter is als ik de taxi neem ik hoop dat ze er snel zijn en dat ze een beetje aardig zijn, eindelijk hij is er laten we snel gaan ik hoop dat die engels spreekt. meneer : Эй, мужик, куда ты хочешь пойти? Я вовсе не похититель Vertaling : yo man waar wil je naartoe ik ben totaal geen ontvoerder hoor. Taco : uhm kan je dat in het engels zeggen nou trouwens laat maar graag naar vodka straat 75C ")
+            time.sleep (10)
+            print ("De bestuurder rijd compleet de verkeerde kant op en je word een beetje bang als je uiteindelijk naar het slot kijkt zie je dat de deuren op slot zitten je vraagt de meneer om te stoppen maar hij gaat juist harder rijden je kijkt naar rechts en je ziet daar vol een vrachtwagen aan komen rijden……...einde")
+
+
+    #stukje 8 (ibiza) einde 1
+scriptinput = input()
+if scriptinput == "C" or scriptinput == "c":
+    os.system("cls")
+    print ("")
+    print ("Goed, rennen!!! Kawod en Taco rennen naar de balie voor tickets. Maar het was al telaat, de tickets waren uitverkocht… Kawod smeekt de receptioniste nog voor de tickets. En ineens vind ze toch nog twee tickets voor het vliegtuig! Kawod en Taco kunnen hun ogen niet geloven! Ze nemen de tickets aan, betalen, en rennen nog naar het vliegtuig. Maar helaas, ze zagen door het raam het vliegtuig opstijgen. Taco begint te huilen, hij zegt: “mmmaar… wat nu?” Toen werd er ineens op Kawod zn schouder geklopt. Het is de politie. Hij zegt dat het hier niet veilig is en we met hem mee moeten. Kawod en Taco kijken elkaar aan, en gaan toch maar mee. Eenmaal aangekomen op het politiebureau, worden ze naar beneden geleid. Kawod vertrouwt dit niet en vraagt: Waar gaat dit heen als ik vragen mag? Ineens worden ze in de boeien geslagen, en in de cel gezet. De politieagent bleek helemaal geen agent te zijn. Maar een martelaar. Hij schreeuwt: “Jullie zijn van mij!! Jullie krijgen niks te eten, en doen wat ik zeg” Taco praat hem nog tegen, maar het werkt niet… Het was al te laat. Kawod kijkt Taco aan en zegt: “Dit was het dan. Hier eindigt ons verhaal.” Later die week werd de martelaar opgepakt, maar bleken Taco en Kawod al dood EINDE")
